fa3af17471da134bca0154a905690bb5832230c4
unknown <ohthatorh@yandex.ru>
2021-11-18 01:19:04 +0300

    upd 18.11

--- src/scripts/catalog.js
+++ src/scripts/catalog.js
@@ -2,7 +2,7 @@ const rangeFilter = document.getElementById('filterRange')
 const inputFilterRange1 = document.getElementById('input-filter-range-1')
 const inputFilterRange2 = document.getElementById('input-filter-range-2')
 const inputs = [inputFilterRange1, inputFilterRange2]
-const listFilters = document.querySelector('.main__catalog-right-filter-list')
+const listFilters = document.querySelector('.main__catalog-applied-list')
 noUiSlider.create(rangeFilter, {
     start: [2000, 150000],
     connect: true,
@@ -18,8 +18,8 @@ rangeFilter.noUiSlider.on('update', function (values, handle) {
         itemFilterPrice[0].children[0].textContent = `${(handle === 0) ? `От ` : `До `} ${Math.round(values[handle]).toLocaleString('ru-RU')}`
     } else {
         const priceFilterElement = `
-            <li class="main__catalog-right-filter-list-item" data-filter="price-${handle}"]">
-                <button class="main__catalog-right-filter-list-item-btn">
+            <li class="main__catalog-applied-list-item" data-filter="price-${handle}"]">
+                <button class="main__catalog-applied-list-item-btn">
                     ${Math.round(values[handle]).toLocaleString('ru-RU')} 
                 </button>
                 <span class="btn-close"></span>
@@ -34,27 +34,53 @@ inputFilterRange1.addEventListener('change', function () {
 inputFilterRange2.addEventListener('change', function () {
     rangeFilter.noUiSlider.set([null, this.value])
 })
-const mainCatalogSwiper = new Swiper(('.main__catalog-swiper'), {
+const mainCatalogSwiper = new Swiper(('.main__catalog-grid-swiper'), {
     spaceBetween: 32,
+    slidesPerColumnFill: 'row',
+    slidesPerColumn: 3,
+    breakpoints: {
+        1025: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+        },
+        769: {
+            spaceBetween: 32,
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+        },
+        320: {
+            spaceBetween: 16,
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+        }
+    },
     pagination: {
         el: '.main__catalog-swiper-pagination',
         clickable: 'true',
         type: 'bullets',
         renderBullet: function (index, className) {
-            return `<span class="${className}">${index+1}</span>`;
+            return `<span class="${className}">${index+1}</span>`
         },
     },
 })
-// const mainCatalogCategory = document.querySelector('.main__catalog-category-choice')
-// const mainChoiceCategory = new Choices(mainCatalogCategory, {
-//     searchEnabled: false,
-//     itemSelectText: '',
-//     classNames: {
-//         // list: 'choices__list-header-bot',
-//         // item: 'choices__item-header-bot'
-//     }
-// })
+
 document.querySelector(`[data-filter="price-0"]`).remove()
+const catalogFilterText = document.querySelector('.main__catalog-grid-text')
+if (window.outerWidth > 1366) {
+    catalogFilterText.textContent = 'Фильтровать по:'
+} else {
+    catalogFilterText.textContent = 'Фильтры:'
+}
+
+const allCategoryFilterElems = Array.from(document.querySelectorAll('.main__catalog-filters-item-checkbox[data-type="category"]'))
+const hideCategoryFilterElems = allCategoryFilterElems.filter(el => getComputedStyle(el).display === 'none')
+const buttonCategoryFilterMore = document.querySelector('.main__catalog-filters-item-more[data-type="category-filter-button"]')
+buttonCategoryFilterMore.textContent = `+ ещё ${hideCategoryFilterElems.length}`
+const allColorFilterElems = Array.from(document.querySelectorAll('.main__catalog-filters-item-checkbox[data-type="color"]'))
+const hideColorFilterElems = allColorFilterElems.filter(el => getComputedStyle(el).display === 'none')
+const buttonColorFilterMore = document.querySelector('.main__catalog-filters-item-more[data-type="color-filter-button"]')
+buttonColorFilterMore.textContent = `+ ещё ${hideColorFilterElems.length}`
+
 document.addEventListener('click', (e) => {
     if (e.target.className === 'btn-close') {
         const filterCheckboxes = document.querySelectorAll(`[data-type="${e.target.parentNode.dataset.filter}"]`)
@@ -65,14 +91,13 @@ document.addEventListener('click', (e) => {
         })
         e.target.parentNode.remove()
     }
-    console.log(e.target)
     if (e.target.className === 'checkbox visually-hidden') {
         if (!e.target.checked) {
             if (e.target.parentNode.textContent.replace(/\s/g, '') === 'Неважно') {
                 const itemFilterSale = document.querySelectorAll('[data-filter="sales"]')
                 itemFilterSale[0].remove()
             } else {
-                const itemFilters = document.querySelectorAll ('.main__catalog-right-filter-list-item')
+                const itemFilters = document.querySelectorAll ('.main__catalog-applied-list-item')
                 itemFilters.forEach(el => {
                     if (el.children[0].textContent.replace(/\s/g, '').includes(e.target.parentNode.textContent.replace(/\s/g, ''))) el.remove()
                 })
@@ -85,8 +110,8 @@ document.addEventListener('click', (e) => {
                 textFilter = (e.target.parentNode.textContent.replace(/\s/g, '') === 'Неважно') ? 'Без скидки' : (!e.target.parentNode.textContent.includes('Более')) ? 'Менее 5000' : 'Более 5000'
             }
             const filterElement = `
-                <li class="main__catalog-right-filter-list-item" data-filter="${e.target.parentNode.dataset.type}">
-                    <button class="main__catalog-right-filter-list-item-btn">
+                <li class="main__catalog-applied-list-item" data-filter="${e.target.parentNode.dataset.type}">
+                    <button class="main__catalog-applied-list-item-btn">
                         ${textFilter}
                     </button>
                     <span class="btn-close"></span>
@@ -95,19 +120,12 @@ document.addEventListener('click', (e) => {
             listFilters.insertAdjacentHTML('beforeend', filterElement)
         }
     }
-    if (e.target.className === 'main__catalog-left-filter-more') {
-        const countItems = (e.target.textContent.includes('12')) ? 13 : 8
-        for (let i = 0; i < countItems; i++) {
-            const filterItem = `
-                <label class="main__catalog-left-filter-checkbox checkbox__label" data-type="category">
-                        <input type="checkbox" name="checkbox" class="checkbox visually-hidden">
-                        <span class="checkbox__span">    
-                            Кровати
-                        </span>
-                </label>
-            `
-            e.target.parentNode.insertAdjacentHTML('beforeend', filterItem)
-        }
+    if (e.target.className === 'main__catalog-filters-item-more') {
+        const selectedFilter = (e.target.dataset.type === 'category-filter-button') ? hideCategoryFilterElems : hideColorFilterElems
+        selectedFilter.forEach(el => el.style.display = 'block')
         e.target.remove()
+    }
+    if (e.target.className === 'main__catalog-filters-item-text') {
+        (e.target.parentNode.classList.contains('filter-active')) ? e.target.parentNode.classList.remove('filter-active') : e.target.parentNode.classList.add('filter-active')
     }
 })
